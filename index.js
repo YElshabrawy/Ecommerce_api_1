514fe79e138df936c2e693969e54c0793a9ae431
Youssef Elshabrawy <yielshabrawy@gmail.com>
2022-03-14 01:12:30 +0200

    auth with encryption

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 //Routers
-const userRouter = require('./Routes/user');
+const userRoute = require("./Routes/user");
+const authRoute = require("./Routes/auth");
 const app = express();
 dotenv.config();
 
 const portNumber = process.env.PORT || 5000;
-app.listen(portNumber, ()=> console.log(`Listening on port ${portNumber}...`));
+app.listen(portNumber, () => console.log(`Listening on port ${portNumber}...`));
 
-mongoose.connect(`mongodb+srv://pasterma:${process.env.MONGO_PASS}@myfirstcluster.2gc2n.mongodb.net/shop?retryWrites=true&w=majority`)
-.then(()=> console.log('Connected to the database'))
-.catch(err => console.error(err));
+mongoose
+  .connect(
+    `mongodb+srv://pasterma:${process.env.MONGO_PASS}@myfirstcluster.2gc2n.mongodb.net/shop?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.error(err));
 
 app.use(express.json());
-app.use('/api/users', userRouter);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
